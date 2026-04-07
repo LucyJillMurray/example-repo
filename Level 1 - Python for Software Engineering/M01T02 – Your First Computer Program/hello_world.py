c27e9961c91eb9b9e742211b28998e1099d144ce
Lucy Murray <murray.lucy04@gmail.com>
2026-04-07 13:04:28 +0200

    Create hello_world.py

---
+++ Level 1 - Python for Software Engineering/M01T02 – Your First Computer Program/hello_world.py
@@ -0,0 +1,14 @@
+"""
+Read in user's name
+Print user's name
+Read in user's age
+Print user's age
+Print hello world
+"""
+name = input("Enter your name: ") 
+print(name)
+
+age = input("Enter your age: ") 
+print(age)
+
+print("Hello, World!")
