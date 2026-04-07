95c83e525f8c4e34de3ee0338ea3635012d183c9
Lucy Murray <murray.lucy04@gmail.com>
2026-04-07 15:35:02 +0200

    Create details.py
    Practice printing

---
+++ Level 1 - Python for Software Engineering/M01T03 – Variables – Storing Data in Programs/details.py
@@ -0,0 +1,6 @@
+#Take in the users details and print them out 
+name = input("Enter your name and surname: ") 
+age = input("Enter your age: ") 
+house_no = input("Enter your house number: ") 
+street_name = input("Enter your street name : ") 
+print(f"This is {name}. They are {age} years old and live at house number {house_no} on {street_name} Street.")
